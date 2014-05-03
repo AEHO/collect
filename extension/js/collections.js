38768490d750900a8dd1eb808527f9ee5286d465
Mateus Zitelli <zitellimateus@gmail.com>
2014-05-03 00:45:17 -0300

    Add collect event and fucntion to get personal id

--- extension/js/collections.js
+++ extension/js/collections.js
@@ -1,10 +1,41 @@
 //Classes dos posts ._4-u2.mbm._5jmm
 
 (function(){
-	$('._4-u2.mbm._5jmm ._5pcq').each(function(){
-		var $this = $(this);
-		if($this.attr('class') === "_5pcq" && $this.attr('rel') === undefined){
-			console.log(this);
-		}
+	var setIcons = function(){
+		$('._4-u2.mbm._5jmm ._5pcq').each(function(){
+			var $this = $(this);
+			if($this.attr('class') === "_5pcq"){
+				var postIdFiler = /posts\/([0-9]*)/;
+				var id = $this.attr('href').match(postIdFiler);
+				if(id){
+					console.log(id[1]);
+				}
+			}
+		});
+	};
+
+	var getMyId = function(){
+		var myIdFilter = /profile_pic_header_([0-9]*)/;
+		var img_id = $("._s0.headerTinymanPhoto._rw.img").attr('id');
+		var myId = img_id.match(myIdFilter)[1];
+		return myId;
+	}
+
+	$('.collect_button').on('click', function(e){
+		e.preventDefault();
+		var data = {
+				userid: myId,
+				postid: $(this).attr('data-postid')
+			};
+
+		$.ajax({
+			type: 'POST',
+			data: data,
+			url: 'https://fb-collect.appspot.com/_ah/api/collectapi/v1/entities'
+		});
 	});
+
+	var myId = getMyId();
+
+	setInterval(setIcons, 1000);
 })();
