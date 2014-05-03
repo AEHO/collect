133b2f6038f7d2a41cb59017488b374aaf26eb0d
Bruno Orlandi <brorlandi@gmail.com>
2014-05-03 00:32:25 -0300

    Adicionando o botão Collect ao lado do Comment.

--- extension/js/collections.js
+++ extension/js/collections.js
@@ -7,4 +7,23 @@
 			console.log(this);
 		}
 	});
+
+	console.log("Facebook post buttons");
+	// cada post 
+	$('._4-u2.mbm._5jmm').each(function(){
+		var $this = $(this);
+		// contem os botões Like, Comment, Share.
+		$this.find("._5pcp._5vsi.lfloat._ohe").each(function(){
+			var a = $(this);
+			// vai incluir o Collect depois do botão de Comment
+			a.find(".uiLinkButton.comment_link").each(function(){
+				var e = $("<a class=\"collect_button\"href=\"#\">Collect</a>"); 
+				$(this).after(e);
+				$(this).after(" · ");
+				console.log($(this));
+			});
+		
+		})
+	});
+
 })();
